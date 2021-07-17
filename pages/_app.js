958bff6951fb5041d61667d80f029b2cfa43228d
Chris <chris@gerrist.nl>
2021-07-17 15:47:28 +0200

    add bootstrap to branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            return <Component {...pageProps} />;
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
